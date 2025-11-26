e54df6d1b8c53936ae9c95a2b7b8a645a0053ad7
Daniel <danielcp62.dc@gmail.com>
2025-11-25 23:46:17 -0500

    Add: Auditoría control tara

--- src/modules/lineas_entrada_salida_service/application/use_cases/control_tara_use_case.py
+++ src/modules/lineas_entrada_salida_service/application/use_cases/control_tara_use_case.py
@@ -1,20 +1,30 @@
-from typing import Optional, List
+from typing import Optional, List, Dict, Any
 
+from src.modules.auth_service.src.application.use_cases.audit_use_case import AuditUseCase
 from src.modules.lineas_entrada_salida_service.application.ports.control_tara import IControlTaraRepository
-from src.modules.lineas_entrada_salida_service.infrastructure.api.schemas.control_tara import TaraCreate
+from src.modules.lineas_entrada_salida_service.infrastructure.api.schemas.control_tara import TaraCreate, TaraResponse
 from src.modules.lineas_entrada_salida_service.domain.entities import ControlTara
 from src.shared.exceptions import AlreadyExistsError
 
 
 class ControlTaraUseCase:
-    def __init__(self, control_tara_repository: IControlTaraRepository):
+    def __init__(self, control_tara_repository: IControlTaraRepository, audit_use_case: AuditUseCase):
         self.control_tara_repository = control_tara_repository
+        self.audit_use_case = audit_use_case
 
-    def create_tara(self, tara_data: TaraCreate) -> ControlTara:
+    def create_tara(self, tara_data: TaraCreate, user_data: Dict[str, Any]) -> ControlTara:
         exist = self.control_tara_repository.exists_by_peso_kg(tara_data.peso_kg)
         if exist:
             raise AlreadyExistsError("Ya existe una tara con este peso asignado")
-        return self.control_tara_repository.create(tara_data)
+        nueva_tara = self.control_tara_repository.create(tara_data)
+        self.audit_use_case.log_action(
+            accion="CREATE",
+            user_id=user_data.get("user_id"),
+            modelo="control_tara",
+            entidad_id=nueva_tara.id,
+            datos_nuevos=TaraResponse.model_validate(nueva_tara).model_dump(mode="json")
+        )
+        return nueva_tara
 
     def get_all_taras(self) -> List[ControlTara]:
         return self.control_tara_repository.get_all()
@@ -22,6 +32,14 @@ class ControlTaraUseCase:
     def get_tara_by_id(self, tara_id: int) -> Optional[ControlTara]:
         return self.control_tara_repository.get_by_id(tara_id)
 
-    #TODO Auditoria
-    def soft_delete(self, tara_id: int) -> bool:
+    def soft_delete(self, tara_id: int, user_data: Dict[str, Any]) -> bool:
+        tara_data = self.control_tara_repository.get_by_id(tara_id)
+        datos_anteriores =  TaraResponse.model_validate(tara_data).model_dump(mode="json")
+        self.audit_use_case.log_action(
+            accion="DELETE",
+            user_id=user_data.get("user_id"),
+            modelo="control_tara",
+            entidad_id=tara_id,
+            datos_anteriores=datos_anteriores
+        )
         return  self.control_tara_repository.soft_delete(tara_id)
--- src/modules/lineas_entrada_salida_service/infrastructure/api/routers/control_tara_router.py
+++ src/modules/lineas_entrada_salida_service/infrastructure/api/routers/control_tara_router.py
@@ -1,22 +1,27 @@
 import logging
+from typing import Dict, Any
 
 from fastapi import APIRouter, status
 from fastapi.params import Depends
-from pyexpat.errors import messages
 from sqlalchemy.orm import Session
 
+from src.modules.auth_service.src.application.use_cases.audit_use_case import AuditUseCase
 from src.modules.lineas_entrada_salida_service.application.use_cases.control_tara_use_case import ControlTaraUseCase
 from src.modules.lineas_entrada_salida_service.infrastructure.api.schemas.control_tara import TaraResponse, TaraCreate
 from src.modules.lineas_entrada_salida_service.infrastructure.db.repositories.control_tara import ControlTaraRepository
 from src.shared.base import get_auth_db
+from src.shared.common.auditoria import get_audit_use_case
 from src.shared.common.responses import success_response, error_response
+from src.shared.security import get_current_user_data
 
 router = APIRouter()
 
 
-def get_tara_use_case(db: Session = Depends(get_auth_db)) -> ControlTaraUseCase:
+def get_tara_use_case(db: Session = Depends(get_auth_db),
+                      audit_uc: AuditUseCase = Depends(get_audit_use_case)) -> ControlTaraUseCase:
     return ControlTaraUseCase(
-        control_tara_repository=ControlTaraRepository(db)
+        control_tara_repository=ControlTaraRepository(db),
+        audit_use_case=audit_uc
     )
 
 
@@ -34,8 +39,9 @@ def get_all_taras(
 def create_tara(
         tara_data: TaraCreate,
         control_tara_use_case: ControlTaraUseCase = Depends(get_tara_use_case),
+        user_data: Dict[str, Any] = Depends(get_current_user_data)
 ):
-    new_data = control_tara_use_case.create_tara(tara_data)
+    new_data = control_tara_use_case.create_tara(tara_data, user_data)
     return success_response(
         data=TaraResponse.model_validate(new_data).model_dump(mode="json"),
         message="Tara creada",
@@ -62,9 +68,10 @@ def get_tara_by_id(
 @router.delete("/{tara_id}", status_code=status.HTTP_200_OK)
 def soft_delete_tara(
         tara_id: int,
-        use_case: ControlTaraUseCase = Depends(get_tara_use_case)
+        use_case: ControlTaraUseCase = Depends(get_tara_use_case),
+        user_data: Dict[str, Any] = Depends(get_current_user_data)
 ):
     return success_response(
-        data=use_case.soft_delete(tara_id),
+        data=use_case.soft_delete(tara_id, user_data),
         message=f"Tara con id {tara_id} eliminada con exito"
     )
